15125cafee99ef57a429169d34638bea4ca3c67f
Dev Sharma <devsharma2057@gmail.com>
2026-06-21 23:42:09 +0530

    05: Social Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,19 @@
-"use client"
+import { auth } from "@/lib/auth"
+import HomeView from "./modules/home/ui/views/Home-view"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-import { authClient } from "@/lib/auth-client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
+const page = async () => {
+   
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-export default function Home() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = () => {
-    authClient.signUp.email({
-      email, 
-      name, 
-      password
-    }, {
-       onError: (ctx) => {
-        console.log("ERROR:", ctx);
-        window.alert("Something went wrong");
-      },
-      onSuccess: (ctx) => {
-        console.log("SUCCESS:", ctx);
-        window.alert("Success");
-      },
-    } );
+  if(!session) {
+    redirect("/sign-in")
   }
 
-  return (
-    <div className="p-4 flex flex-col gap-y-6">
-      <Input
-        placeholder="Enter Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholder="Enter Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="Enter Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={onSubmit}>
-        Submit
-      </Button>
-    </div>
-  );
+  return <HomeView />
 }
+
+export default page
